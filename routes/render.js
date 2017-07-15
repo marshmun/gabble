bfc78a7dae5359eb44e0ff32734d72bf2362098b
Marshall Munoz <marshmun@gmail.com>
2017-07-15 11:48:31 -0500

    added an additonal profile page that has the specific users gabs

--- routes/render.js
+++ routes/render.js
@@ -25,6 +25,9 @@ function render(app) {
     app.get('/profile', checkAuth, function (req, res) {
         res.render('profile')
     })
+    app.get('/myGabs', checkAuth, function (req, res) {
+        res.render('personalGabs')
+    })
 
     app.get('/homepage', checkAuth, function (req, res) {
         models.post.findAll({
